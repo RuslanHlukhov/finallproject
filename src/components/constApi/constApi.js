b341ccaa576bcc4c5da5d36375c3ebddfea2d7e6
Руслан <ruslanhlukhov@gmail.com>
2021-11-15 13:31:31 +0300

    api

--- src/components/constApi/constApi.js
+++ src/components/constApi/constApi.js
@@ -1,13 +1,11 @@
 import { Axios } from "axios";
 
 const dbUrl = 'https://backendforfinallproject.herokuapp.com/api/'
-export const addTest = () => {
-    Axios.post(`${dbUrl}users`, {
-        title: title,
-        text: text,
-    }).then((response) => {
+export const addTest = (res) => {
+    Axios.post(`${dbUrl}users`, res)
+    .then((response) => {
         console.log(response);
-    }).catch(error => {
-        console.log(error.response);
+    }).catch((error) => {
+        console.log(error);
     })
 }
